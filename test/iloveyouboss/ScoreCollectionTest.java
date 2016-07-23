12ff9a43fa3f3811bddced87aa502988bbee49eb
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-07-23 09:11:41 -0700

    Add null and divide by zero checks to ScoreCollection

--- test/iloveyouboss/ScoreCollectionTest.java
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -5,15 +5,38 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.*;
 
 public class ScoreCollectionTest {
+    private ScoreCollection collection;
+
+    @Before
+    public void setup() {
+        collection = new ScoreCollection();
+    }
 
     @Test
     public void answersArithmeticMeanOfTwoNumbers() {
-        ScoreCollection collection = new ScoreCollection();
         collection.add(() -> 5);
         collection.add(() -> 7);
 
         int actualResult = collection.arithmeticMean();
 
         assertThat(actualResult, is(equalTo(6)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionWhenAddingNull() {
+        collection.add(null);
+    }
+
+    @Test
+    public void answersZeroWhenNoElementsAdded() {
+        assertThat(collection.arithmeticMean(), equalTo(0));
+    }
+
+    @Test
+    public void dealsWithIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertThat(collection.arithmeticMean(), equalTo(1073741824));
     }
 }
